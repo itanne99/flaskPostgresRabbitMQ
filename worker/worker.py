e47e063ddc9a965b2d85a2bd4b67c1865b75a9ae
Ido Tanne <idovader@gmail.com>
2022-02-07 10:52:45 -0500

    Added basic "add person" to db using rabbit

--- worker/worker.py
+++ worker/worker.py
@@ -1,4 +1,7 @@
 import pika
+from sqlalchemy import create_engine, Column, String, Integer, Identity
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 '''
 Code Block: Wait for server to turn on
@@ -11,20 +14,41 @@ print(' [*] Sleeping for %i seconds.' % sleepTime)
 time.sleep(sleepTime)
 '''
 
-
-print(' [*] Connecting to server ...')
+# rabbitMQ connection
+print('[*] Connecting to server ...')
 connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbit-flask'))
 channel = connection.channel()
 channel.queue_declare(queue='task_queue', durable=True)
+
+# PostgreSQL connection
+print('[*] Connecting to database ...')
+db_string = "postgresql://rabbit:rabbitmqDatabase@postgres-flask:5432/rabbitTasks"
+
+db = create_engine(db_string)
+base = declarative_base()
+Session = sessionmaker(db)
+session = Session()
+
+
+# DB Model
+class Person(base):
+    __tablename__ = 'people'
+    id = Column(Integer, Identity(start=1, cycle=True), primary_key=True, )
+    name = Column(String)
+
+
+# Create Model/Tables
+base.metadata.create_all(db)
 
 print('[*] Waiting for messages.')
 
 
 def callback(ch, method, properties, body):
     print("[x] Received %s" % body)
-    cmd = body.decode()
-    print(cmd)
-
+    name = body.decode()
+    new_person = Person(name=name)
+    session.add(new_person)
+    session.commit()
     print('[x] Done')
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
